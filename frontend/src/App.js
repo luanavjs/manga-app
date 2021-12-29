5f05a5779593377343962560429e5ffb5f5873a9
Luana Vallejos <luanalorenavallejos@gmail.com>
2021-12-29 19:05:14 -0300

    Agrego rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,19 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+import BookDetailsView from './views/BookDetailsView';
+import HomeView from "./views/HomeView";
 
 function App() {
-  const apiUrl = process.env.REACT_APP_API_URL
-  const [books, setBooks] = useState([])
-
-  useEffect(() => {
-    getBooks()
-  }, [])
-
-  async function getBooks() {
-    const response = await axios.get(`${apiUrl}/books`)
-    setBooks(response.data)
-  }
-
+  
   return (
-    <div className="container mt-5">
-     <h1>Manga App</h1>
-     {books.map(book =>(
-       <h3 key={book._id}>{book.title}</h3>
-     ))}
-    </div>
-  );
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<HomeView/>}/>
+        <Route path='/book/:id' element={<BookDetailsView/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
