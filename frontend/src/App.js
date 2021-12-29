79ba9c60c4e131c6797a2ffe01ae905f1babbfd0
Luana Vallejos <luanalorenavallejos@gmail.com>
2021-12-29 00:33:40 -0300

    utilizo hooks y axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    getBooks()
+  }, [])
+
+  async function getBooks() {
+    const response = await axios.get(`${apiUrl}/books`)
+    setBooks(response.data)
+  }
+
   return (
-    <div className="App">
-     <h1>Hola mundo</h1>
+    <div className="container mt-5">
+     <h1>Manga App</h1>
+     {books.map(book =>(
+       <h3 key={book._id}>{book.title}</h3>
+     ))}
     </div>
   );
 }
