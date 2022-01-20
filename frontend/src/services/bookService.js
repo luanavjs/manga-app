5b3edbbff6444213cc68be6a399cdf36eb11ba00
Luana Vallejos <luanalorenavallejos@gmail.com>
2022-01-20 13:41:26 -0300

    refactorizo y agrego peticiones con axios

--- frontend/src/services/bookService.js
+++ frontend/src/services/bookService.js
@@ -1,10 +1,22 @@
-import axios from "axios";
+import axios from  "axios";
 
 const apiUrl = process.env.REACT_APP_API_URL
 
-const createBook = (bookObj) => {
+export const getAllBooks = async () => {
+    const response = await axios.get(`${apiUrl}/books`)
+    return response
+}
+
+export const getBookById = async (id) => {
+    const response = await axios.get(`${apiUrl}/books/book/${id}`)
+    return response
+}
+
+export const createBook = (bookObj) => {
     const response = axios.post(`${apiUrl}/books/book`, bookObj)
     return response
 }
 
-export default createBook;
+export const editBook = (bookObj,id) => {
+    const response = axios.put(`${apiUrl}/books/book/${id}`, bookObj)
+}
