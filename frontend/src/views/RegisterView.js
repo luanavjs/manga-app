c6399a3d9c63080e451afd346a70a3b46750fcf1
Luana Vallejos <luanalorenavallejos@gmail.com>
2022-01-20 13:42:41 -0300

    creo formulario para registro

--- frontend/src/views/RegisterView.js
+++ frontend/src/views/RegisterView.js
@@ -1,22 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { register } from "../services/authService";
 
 const RegisterView = () => {
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (event) => {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const response = await register(user)
+        setUser({
+            name: "",
+            email: "",
+            password: ""
+        })
+        navigate('/')
+    }
+
     return (
         <div className="container mt-5">
-          <form className="form">
+          <form onSubmit={handleSubmit} className="form">
           <h2>Registro</h2>
             <input
+                onChange={handleChange}
+                name="name"
+                value={user.name}
                 className="form-control" 
                 type="text" 
                 placeholder="Nombre"
             />
-            <input 
+            <input
+                onChange={handleChange} 
+                name="email"
+                value={user.email}
                 className="form-control" 
                 type="text" 
                 placeholder="Email"
             />
-            <input 
+            <input
+                onChange={handleChange} 
+                name="password"
+                value={user.password}
                 className="form-control" 
-                type="text" 
+                type="password" 
                 placeholder="Contraseña"
             />
             <button className="btn btn-outline-dark form-control">Registrarse</button>
